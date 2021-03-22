341ffcdc3dfef9a298244be1bce92ce9bfa871b4
shakera <sanderso_4516574@yahoo.com>
2021-03-21 21:17:45 -0400

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Shakera is the champion"
+
+document.getElementsByTagName("body")[0].appendChild(newHeader.innerHTML)
